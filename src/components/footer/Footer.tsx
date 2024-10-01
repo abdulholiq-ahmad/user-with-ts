b5e4002d2aa64b08b53efa6df282b5009c1871d4
Ahmadbekov Abdulholiq <abdulholiq571@gmail.com>
2024-10-02 00:25:43 +0500

    add: footer and header

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,7 @@
+import { FC } from "react";
+
+const Footer: FC = () => {
+  return <div className="text-3xl">Footer</div>;
+};
+
+export default Footer;
---
+++ src/layout/index.tsx
@@ -0,0 +1,16 @@
+import { FC } from "react";
+import Header from "../components/header/Header";
+import { Outlet } from "react-router-dom";
+import Footer from "../components/footer/Footer";
+
+const Index: FC = () => {
+  return (
+    <>
+      <Header />
+      <Outlet />
+      <Footer />
+    </>
+  );
+};
+
+export default Index;
